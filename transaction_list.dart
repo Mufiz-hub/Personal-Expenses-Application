6b8b4efa2b561588714935fd1cde65f910005006
Mufiz-hub <127823480+Mufiz-hub@users.noreply.github.com>
2023-07-04 10:22:14 -0700

    Update transaction_list.dart
    Fix Some Bug

--- transaction_list.dart
+++ transaction_list.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
-import '../models/transaction.dart';
+import './transaction.dart';
 
 class TransactionList extends StatelessWidget {
   final List<Transaction> transactions;
